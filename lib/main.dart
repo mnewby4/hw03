429345018b3f298752d47b312befcd25f846d778
mylanewby <167246119+mylanewby@users.noreply.github.com>
2025-03-13 01:15:36 -0400

    Added flipping animatedbuilder + restructured code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'helper.dart';
-import 'dart:math';
+import 'dart:math' as math;
 DatabaseHelper myHelper = DatabaseHelper();
 /*
   Objective: card-matching game w animation+state management, player shld match cards from
@@ -13,8 +13,8 @@ DatabaseHelper myHelper = DatabaseHelper();
     Xdata model for cards with 
       Xfront+back design properties
       Xcurrent state [face up or down]
-  animation:   animatedbuilder or animatedcontainer
-    flip from down -> up and vice versa
+  Xanimation:   animatedbuilder or animatedcontainer
+   X flip from down -> up and vice versa
   Xgame logic: track if currently up or down
     Xwhen player taps 2 face-down cards, check if they match
       XMATCH -> keep face up
@@ -77,149 +77,150 @@ class Card {
   
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
   List<Card> sessionCards = [];
   List<int> takenNums = [];
   int cardsMax = 16;
   List<Card> matchedCards = [];
   List<Card> tappedCards = [];
   String message = "Find all the matching pairs!";
+  
+  late AnimationController _controller;
+  late Animation<double> _animation;
 
   @override
   void initState() {
     super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+    _animation = Tween<double>(begin: 0, end: 1).animate(_controller)
+      ..addListener(() {
+        setState(() {}); 
+      });
     _generateCards();
   }
 
-  void _generateCards() async {
-    //setState(() {
-      //generate 8 different numbers, create them 2x -> 16 total
-      Random random = new Random();
-      for (int i = 0; sessionCards.length < cardsMax; i++) {
-        //make each property so they can be created 2x
-        //pick random from 1-8 [ids 1-13] and ALSO make sure that num ISNT alrdy taken
-        int randomNum = random.nextInt(13) + 1;
-        if (!takenNums.contains(randomNum)) {
-          takenNums.add(randomNum);
-          Card? currentCard = await myHelper.queryOneRow(randomNum);
-          if (currentCard != null) {
-            sessionCards.add(
-              Card(
-                cardID: currentCard.cardID, 
-                isCardUp: currentCard.isCardUp,
-                frontDesign: currentCard.frontDesign, 
-                backDesign: currentCard.backDesign,
-                duplicateId: sessionCards.length,
-              )
-            );
-            sessionCards.add(
-              Card(
-                cardID: currentCard.cardID, 
-                isCardUp: currentCard.isCardUp,
-                frontDesign: currentCard.frontDesign, 
-                backDesign: currentCard.backDesign,
-                duplicateId: sessionCards.length,
-              )
-            );
-          }
-        }
-      }
-        sessionCards.shuffle();
-        setState(() {});
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
-  void _flipCards() async {
+  Future<void> _generateCards() async {
+    math.Random random = math.Random();
+    while (sessionCards.length < cardsMax) {
+      int randomNum = random.nextInt(13) + 1;
+      if (!takenNums.contains(randomNum)) {
+        takenNums.add(randomNum);
+        Card? currentCard = await myHelper.queryOneRow(randomNum);
+        if (currentCard != null) {
+          sessionCards.addAll([
+            Card(
+              cardID: currentCard.cardID,
+              isCardUp: 0,
+              frontDesign: currentCard.frontDesign,
+              backDesign: currentCard.backDesign,
+              duplicateId: sessionCards.length,
+            ),
+            Card(
+              cardID: currentCard.cardID,
+              isCardUp: 0,
+              frontDesign: currentCard.frontDesign,
+              backDesign: currentCard.backDesign,
+              duplicateId: sessionCards.length + 1,
+            ),
+          ]);
+        }
+      }
+    }
+    sessionCards.shuffle();
+    setState(() {});
+  }
+
+  void _flipCard(int index) {
     setState(() {
-      
+      if (tappedCards.length >= 2 || matchedCards.contains(sessionCards[index]) || tappedCards.contains(sessionCards[index])) {
+        return;
+      }
+      sessionCards[index].isCardUp = (sessionCards[index].isCardUp == 1) ? 0 : 1;
+      tappedCards.add(sessionCards[index]);
+      if (tappedCards.length == 2) {
+        int firstIndex = tappedCards[0].duplicateId;
+        int secondIndex = tappedCards[1].duplicateId;
+        if (tappedCards[0].cardID == tappedCards[1].cardID) {
+          message = "Match found!";
+          matchedCards.add(sessionCards[firstIndex]);
+          matchedCards.add(sessionCards[secondIndex]);
+          tappedCards.clear();
+        } else {
+          Future.delayed(const Duration(seconds: 1), () {
+            setState(() {
+              tappedCards[0].isCardUp = 0;
+              tappedCards[1].isCardUp = 0;
+              tappedCards.clear();
+              message = "Try again.";
+            });
+          });
+        }
+        if (matchedCards.length == cardsMax) {
+          message = "You won! Congrats!!";
+        }
+      }
     });
   }
 
-  Future<List<Map<String, dynamic>>> _display() async {
-    return await myHelper.queryAllRows();
-  }
-  Future<int> _countReturn() async {
-    return await myHelper.queryRowCount();
+  Widget _buildCard(int index) {
+    bool isFaceUp = sessionCards[index].isCardUp == 1;
+    return GestureDetector(
+      onTap: () => _flipCard(index),
+      child: AnimatedBuilder(
+        animation: _animation,
+        builder: (context, child) {
+          return Transform(
+            transform: Matrix4.rotationY(isFaceUp ? 0 : math.pi),
+            alignment: Alignment.center,
+            child: Container(
+              width: 80,
+              height: 90,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: isFaceUp
+                  ? Image.network(sessionCards[index].frontDesign)
+                  : Image.network(sessionCards[index].backDesign),
+            ),
+          );
+        },
+      ),
+    );
   }
 
   @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      title: Text(widget.title),
-    ),
-    body: Center(
-      child: Column(
-        children: [
-          Text("\n$message\n\n",
-          style: Theme.of(context).textTheme.headlineLarge),
-          Expanded(
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text("\n$message\n\n",
+                style: Theme.of(context).textTheme.headlineLarge),
+            Expanded(
               child: GridView.builder(
-                shrinkWrap: true,
                 itemCount: sessionCards.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 4,
                 ),
-                itemBuilder: (context, duplicateId) {
-                  return SingleChildScrollView(
-                    child: Column(
-                      children: [    
-                        Image.network(
-                          sessionCards[duplicateId].isCardUp == 1
-                              ? sessionCards[duplicateId].frontDesign
-                              : sessionCards[duplicateId].backDesign,
-                          width: 80,
-                          height: 90,
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            //if the card they just clicked on is already in matchedCards, return
-                            /*for (int i = 2; i <= matchedCards.length; i++) {
-                              if (matchedCards[i] == sessionCards[duplicateId]) {
-                                return;
-                              }
-                            }*/
-                            if (tappedCards.length >= 2) {
-                              return;
-                            } else {
-                              setState(() {
-                                if (matchedCards.contains(sessionCards[duplicateId]) || tappedCards.contains(sessionCards[duplicateId])) {
-                                  return;
-                                }
-                                sessionCards[duplicateId].isCardUp = (sessionCards[duplicateId].isCardUp == 1) ? 0 : 1;
-                                tappedCards.add(sessionCards[duplicateId]);
-                                if (tappedCards.length == 2) {
-                                  int firstIndex = tappedCards.length - 2;
-                                  int secondIndex = tappedCards.length - 1;
-                                  if (tappedCards[firstIndex].cardID == tappedCards[secondIndex].cardID) {
-                                    message = "Match found!";
-                                    matchedCards.add(tappedCards[0]);
-                                    matchedCards.add(tappedCards[1]);
-                                    tappedCards.clear();
-                                  } else {
-                                    tappedCards[tappedCards.length - 1].isCardUp = 0;
-                                    tappedCards[tappedCards.length - 2].isCardUp = 0;
-                                    tappedCards.clear();
-                                    //numMatched = 0;
-                                    message = "Try again.";                            
-                                  }
-                                  if (matchedCards.length == 16) {
-                                    message = "You won! Congrats!!";
-                                  }
-                                }
-                              });
-                            }
-                          },
-                        ),
-                      ],
-                    ),
-                  );
-                },
+                itemBuilder: (context, index) => _buildCard(index),
               ),
             ),
-        ],
+          ],
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 }
